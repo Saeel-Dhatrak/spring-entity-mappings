9e39a9e3d19d59355bd1fd7e670db41dc691a7fd
saeel.dhatrak <saeeldhatrak007@gmail.com>
2026-05-25 09:24:46 +0530

    One to Many Mapping : Post is the owner of relationship

---
+++ src/main/java/com/example/socialmedia/mappings/models/Post.java
@@ -0,0 +1,14 @@
+package com.example.socialmedia.mappings.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private SocialUser socialUser;
+}
